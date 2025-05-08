43562aa4f6a3b2d3cbbaf3058aab991c00c55774
Eduardo Hernandez <ppjehg@gmail.com>
2025-05-08 15:18:14 +0300

    feat: add binary iterative

--- JS/search/binary.js
+++ JS/search/binary.js
@@ -1,8 +1,20 @@
-// JavaScript program to implement recursive Binary Search
+/**
+ * JavaScript program to implement recursive Binary Search
+ *
+ * A recursive binary search function. It returns
+ * location of x in given array arr[l..r] is present,
+ * otherwise -1
+ *
+ * Time Complexity	O(log n)
+ * Space Complexity	O(1)
+ *
+ * @param {*} array
+ * @param {*} left
+ * @param {*} right
+ * @param {*} searchTerm
+ * @returns
+ */
 
-// A recursive binary search function. It returns
-// location of x in given array arr[l..r] is present,
-// otherwise -1
 function binarySearch(array, left, right, searchTerm) {
   if (right >= left) {
     let mid = left + Math.floor((right - left) / 2);
@@ -31,5 +43,49 @@ let x = 10;
 let n = arr.length;
 let result = binarySearch(arr, 0, n - 1, x);
 result == -1
-  ? document.write("Element is not present in array")
-  : document.write("Element is present at index " + result);
+  ? console.log("Element is not present in array")
+  : console.log("Element is present at index " + result);
+
+function binarySearchIterative(orderedList, target) {
+  let start = 0;
+  let end = orderedList.length - 1;
+
+  while (start <= end) {
+    let midpoint = Math.floor((end + start) / 2);
+    let currentNumber = orderedList[midpoint];
+    if (currentNumber === target) {
+      return midpoint;
+    } else if (currentNumber < target) {
+      start = midpoint + 1;
+    } else {
+      end = midpoint - 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearchIterative([1, 2, 3, 4, 5], 1));
+console.log(binarySearchIterative([1, 2, 3, 4, 5], 5));
+console.log(binarySearchIterative([1, 2, 3, 4, 5], 3));
+console.log(binarySearchIterative([1, 2, 3, 4, 5], 63));
+
+function binarySearchRec(orderedList, target) {
+  if (orderedList.length < 1) {
+    return -1;
+  }
+
+  let midpoint = Math.floor(orderedList.length / 2);
+  if (orderedList[midpoint] === target) {
+    return midpoint;
+  } else if (orderedList[midpoint] < target) {
+    return binarySearchRec(
+      orderedList.slice(midpoint + 1, orderedList.length),
+      target
+    );
+  } else {
+    return binarySearchRec(orderedList.slice(0, midpoint), target);
+  }
+}
+console.log(binarySearchRec([1, 2, 3, 4, 5], 1));
+console.log(binarySearchRec([1, 2, 3, 4, 5], 5));
+console.log(binarySearchRec([1, 2, 3, 4, 5], 3));
+console.log(binarySearchRec([1, 2, 3, 4, 5], 63));
